1b7b412b51e96b372840653b1a8d1318ca31b995
taeyang2 <taeyang2.lee@gmail.com>
2021-02-06 06:53:06 +0900

    세부정보 페이지 DB 연결

--- app/src/main/java/com/example/thecheck/DetailActivity.java
+++ app/src/main/java/com/example/thecheck/DetailActivity.java
@@ -43,41 +43,36 @@ public class DetailActivity extends AppCompatActivity{
 
         dbHelper = new DBHelper(this, dbName, null, dbVersion);
 
-
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
         SQLiteDatabase db;
         String sql;
 
-        if (requestCode == 1) {
-            if(resultCode == RESULT_OK) {
-                db = dbHelper.getReadableDatabase();
-                sql = "SELECT * FROM class;";
-                Cursor cursor = db.rawQuery(sql, null);
-                if (cursor.getCount() > 0) {
-                    while (cursor.moveToNext()){
-                        lectName2.setText(cursor.getString(1));
-                        lectTerm2.setText(cursor.getString(6));
-                        lectType2.setText(cursor.getString(2));
-                        lectUrl2.setText(cursor.getString(5));
+        db = dbHelper.getReadableDatabase();
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("이름");
+        sql = "SELECT * FROM class Where lectName = '"+message+"';";
 
-                    }
+        long now = System.currentTimeMillis();
+        java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd");
+        String date = sdf.format(new java.util.Date(now));
 
-                } else {
-                    return;
-                }
-                cursor.close();
+        Cursor cursor = db.rawQuery(sql, null);
+        if (cursor.getCount() > 0) {
+            while (cursor.moveToNext()){
+                lectName2.setText(cursor.getString(1));
+                lectTerm2.setText(date+"~"+cursor.getString(6));
+                lectType2.setText(cursor.getString(2));
+                lectUrl2.setText(cursor.getString(5));
 
             }
 
-            dbHelper.close();
+        } else {
+            return;
         }
+        cursor.close();
+        dbHelper.close();
+
     }
+
 
     // DBHelper 클래스 (taeyang2.lee)
     static class DBHelper extends SQLiteOpenHelper {
@@ -93,7 +88,7 @@ public class DetailActivity extends AppCompatActivity{
         public void onCreate(SQLiteDatabase db){
             db.execSQL("CREATE TABLE IF NOT EXISTS class " +
                     "(_id INTEGER PRIMARY KEY AUTOINCREMENT, lectName TEXT, lectType TEXT, " +
-                    "dueDate INTEGER, lecStartDate TEXT, lessons INTEGER, url VARCHAR(20));");
+                    "lecStartDate TEXT, lessons INTEGER, url VARCHAR(20), lastDate TEXT, curnum INTEGER);");
         }
 
         // DB 업데이트 시 호출 (taeyang2.lee)
