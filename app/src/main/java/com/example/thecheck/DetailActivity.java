3adfff82f4abf3e2d9952d76bf8b169c255f198d
jwc406 <jw.choi406@gmail.com>
2021-02-06 00:52:38 +0900

    개인작업

--- app/src/main/java/com/example/thecheck/DetailActivity.java
+++ app/src/main/java/com/example/thecheck/DetailActivity.java
@@ -1,12 +1,13 @@
 package com.example.thecheck;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DetailActivity extends AppCompatActivity{
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_detail);
     }
